d0d0fbeafdb18b6a5dda6b5462d686b5e4292336
nchoquet <nicolachoquet06250@gmail.com>
2026-01-26 17:33:58 +0100

    amélioration de la salle de démo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { defineCustomElement } from 'vue'
 import RoomBuilder from './components/RoomBuilder.vue'
+import dataJson from './assets/data.json';
 
 const RoomBuilderElement = defineCustomElement(RoomBuilder)
 
 customElements.define('room-builder', RoomBuilderElement)
 
 window.addEventListener('DOMContentLoaded', () => {
+    const roomBuilder = document.createElement('room-builder');
+    roomBuilder.setAttribute('layers', JSON.stringify(dataJson))
+    roomBuilder.setAttribute('room-id', '1')
+    roomBuilder.setAttribute('room-name', 'Salle principale')
+
+    // @ts-ignore
+    document.body.appendChild(roomBuilder);
+
     document.querySelector('room-builder')?.addEventListener('saved', event => {
         console.log('Room saved:', event.detail[0])
     })
